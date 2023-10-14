43976694b52c37a388015996eaf02149cbdc2d76
kittyyliu <kittyliu113@gmail.com>
2023-10-14 15:14:31 -0700

    changes to homepage

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,14 +1,29 @@
 import headshot from '../../assets/headshot.jpg';
 
 const Home = () => {
-    return ( 
-        <div className="Home">
-            <h2>software + </h2>
-            <h2>software + </h2> 
-            <img src={headshot} alt="kittyliu" 
-            style={{ width: '150px', height: '150px', borderRadius: '50%', objectFit: 'cover' }}
-            />
-        </div>
-    );
-}
+  return (
+    <div className="Home">
+      <h1>
+        software <span className="pink-text">& product</span>
+      </h1>
+      <img
+        src={headshot}
+        alt="kittyliu"
+        style={{
+          padding: '20px',
+          width: '150px',
+          height: '150px',
+          borderRadius: '50%',
+          objectFit: 'cover',
+        }}
+      />
+      <h2>
+        hello! i'm <span className="kitty">kitty</span>. i've always been interested in the dynamic intersection between
+        technology, creativity, and people, which ultimately led me to product management. here, you'll find stories about
+        my journey in tech, my hobbies, and places i've traveled.
+      </h2>
+    </div>
+  );
+};
+
 export default Home;
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,16 +1,53 @@
+import React, { useState } from 'react';
+
 const Navbar = () => {
-    return (
+    const [activeLink, setActiveLink] = useState('home');
+    const handleLinkClick = (link) => {
+    setActiveLink(link);
+};
+return (
+    
     <nav className="navbar">
-    <h1>kittyliu.xyz</h1>
-    <div className="links">
-        <a href="/">home</a>
-        <a href="/about">about</a>
-        <a href="/projects">projects</a>
-        <a href="/resume">resume</a>
-        <a href="/art">fun</a>
-        </div>
+      <h1>kittyliu.xyz</h1>
+      <div className="links">
+        <a
+          href="/"
+          className={activeLink === 'home' ? 'active' : ''}
+          onClick={() => handleLinkClick('home')}
+        >
+          Home
+        </a>
+        <a
+          href="/about"
+          className={activeLink === 'about' ? 'active' : ''}
+          onClick={() => handleLinkClick('about')}
+        >
+          About
+        </a>
+        <a
+          href="/projects"
+          className={activeLink === 'projects' ? 'active' : ''}
+          onClick={() => handleLinkClick('projects')}
+        >
+          Projects
+        </a>
+        <a
+          href="/resume"
+          className={activeLink === 'resume' ? 'active' : ''}
+          onClick={() => handleLinkClick('resume')}
+        >
+          Resume
+        </a>
+        <a
+          href="/art"
+          className={activeLink === 'art' ? 'active' : ''}
+          onClick={() => handleLinkClick('art')}
+        >
+          Fun
+        </a>
+      </div>
     </nav>
-    );
-}
+  );
+};
 
 export default Navbar;
