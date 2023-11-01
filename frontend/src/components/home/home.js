aad87c68e836bf798d1249237134487166ef8ebd
kittyyliu <kittyliu113@gmail.com>
2023-11-01 15:49:45 -0700

    new homepage changes

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -20,7 +20,7 @@ const Home = () => {
       />
       <h2>
         hello! i'm <a href="https://en.wikipedia.org/wiki/Hello_Kitty" className="kitty" target="_blank">kitty</a>. 
-        i've always been interested in the dynamic intersection between <a href="/tech" className="kitty">technology</a>, <a href="/fun" className="kitty">creativity</a>, and <span className="kitty">people</span>, which ultimately led me to <span className="kitty">product management</span>. 
+        i've always been interested in the dynamic intersection between <a href="/tech" className="kitty" target="_blank">technology</a>, <a href="/fun" className="kitty">creativity</a>, and <span className="kitty">people</span>, which ultimately led me to <span className="kitty">product management</span>. 
         here, you'll find stories about my <span className="kitty">journey in tech</span>, my <span className="kitty">hobbies</span>, and <span className="kitty">places i've traveled</span>. happy exploring!
       </h2>
     </div>
