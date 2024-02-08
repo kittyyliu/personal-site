17082427001a7bff0a1a1191182a225d8945b23a
kittyyliu <kittyliu113@gmail.com>
2024-02-08 13:31:56 -0800

    added all projects

--- frontend/src/components/portfolio/portfolio.js
+++ frontend/src/components/portfolio/portfolio.js
@@ -7,13 +7,14 @@ import verses from '../../assets/verses.png';
 
 const Portfolio = () => {
     const [selectedCategory, setSelectedCategory] = useState('software');
+    const [activeButton, setActiveButton] = useState('software');
   
     const projects = [
       {
         category: 'software',
         image: ssb,
         tech: 'SQL, Node.js, React.js, Express.js',
-        title: 'Super Smash Bros. Tournament Tracker',
+        title: 'Super Smash Bros. Tournament Tracker (CPSC 304 Project)',
         description: 'Created a web application to manage and track Super Smash Bros. tournaments, allowing users to add tournaments, filter for tournaments based on players, country, wins, or losses, and delete players ',
         link: 'https://github.com/kittyyliu/304project'
       },
@@ -37,10 +38,29 @@ const Portfolio = () => {
         category: 'software',
         image: pawndr,
         tech: 'SQL, Node.js, React.js, Express.js',
-        title: 'Pawndr',
+        title: 'Pawndr (Hack Western X Winner)',
         description: `Collaborated with a team of 3 other software developers to build an AI-powered full-stack web application in 36 hours, placing in the top 5
         projects overall, as well as winning 3 other awards of Best Mental Health Hack,
         Most Creative Use of GitHub, and Organizer's Choice Award`,
+        link: 'https://github.com/kittyyliu/304project'
+      },
+      {
+        category: 'software',
+        image: "",
+        tech: 'Java, Java Swing',
+        title: 'BetBank (CPSC 210 Project)',
+        description: `Developed an application to simulate a sports-betting bank using Java, allowing users to deposit, bet, check their balance, and check their transaction history. 
+        Persisted data to memory file in JSON format, allowing users to save and load data as a JSON file. 
+        Designed a graphical user interface (GUI) using Java Swing with elements such as a splash screen and interactive popups`,
+        link: 'https://github.com/kittyyliu/BetBank'
+      },
+      {
+        category: 'product',
+        image: straysafe,
+        tech: 'Java, Java Swing',
+        title: 'StraySafe (DTTP Product Hackathon)',
+        description: `Led the ideation, research, design, and pitch of StraySafe, an AI-powered web app allowing users to post lost/found pets and facilitates pet reunions through photo similarity matching. 
+        Worked cross-functionally with a team of 3 other product managers, overseeing project management, task assignment, and executing the entire design process`,
         link: 'https://github.com/kittyyliu/304project'
       },
     ];
@@ -52,18 +72,42 @@ const Portfolio = () => {
         <header>
           <h1>Portfolio</h1>
           <div className="toggles">
-            <button onClick={() => setSelectedCategory('software')}>Software</button>
-            <button onClick={() => setSelectedCategory('design')}>Design</button>
-            <button onClick={() => setSelectedCategory('product')}>Product</button>
-          </div>
+          <button
+            onClick={() => {
+              setSelectedCategory('software');
+              setActiveButton('software');
+            }}
+            className={activeButton === 'software' ? 'active' : ''}
+          >
+            Software
+          </button>
+          <button
+            onClick={() => {
+              setSelectedCategory('design');
+              setActiveButton('design');
+            }}
+            className={activeButton === 'design' ? 'active' : ''}
+          >
+            Design
+          </button>
+          <button
+            onClick={() => {
+              setSelectedCategory('product');
+              setActiveButton('product');
+            }}
+            className={activeButton === 'product' ? 'active' : ''}
+          >
+            Product
+          </button>
+        </div>
         </header>
   
         <section className="projects">
           {filteredProjects.map((project, index) => (
             <article className="project" key={index}>
-              <h2>{project.title}</h2>
-              <p>{project.tech}</p>
-              <p>{project.description}</p>
+              <h2 className='title'>{project.title}</h2>
+              <p className='tech'>{project.tech}</p>
+              <p className='description'>{project.description}</p>
               <div className='image-container'>
               <img src={project.image}></img>
               </div>
